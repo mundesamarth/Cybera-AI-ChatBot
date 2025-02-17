e39f5b5f4020491d12b1703392534264b8175a87
Sam <gentalmen28@gmail.com>
2025-02-17 01:58:33 +0000

    Setting up the route and changes in the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import './index.css';
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Dashboard from './routes/dashboard/Dashboard';
+import Chatpage from './routes/chatPage/Chatpage';
+import Homepage from './routes/homepage/homepage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/dashboard",
+    children:[{path:"/dashboard",element:<Dashboard/>},
+      {path:"/dashboard/chats/:id", element:<Chatpage/>}]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
