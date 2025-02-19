627ca804eeefc6e24e59b98c1479c51becda4bbb
Sam <gentalmen28@gmail.com>
2025-02-19 17:29:44 +0000

    Created image recognition and setup the streaming in newpromt.jsx

--- FrontEnd/src/components/newPrompt/NewPrompt.jsx
+++ FrontEnd/src/components/newPrompt/NewPrompt.jsx
@@ -13,8 +13,26 @@ const NewPrompt = () => {
   const [img,setImg] = useState({
     isLoading:false,
     error:"",
-    dbData:{}
+    dbData:{},
+    aiData:{}
   })
+
+  
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig:{
+
+    },
+  });
 
   const endRef = useRef(null);
 
@@ -24,8 +42,22 @@ const NewPrompt = () => {
 
   const add = async (text) => {
   setQuestion(text);
-  const result = await model.generateContent(text);
-  setAnswer(result.response.text())
+  const result = await chat.sendMessageStream(
+    Object.entries(img.aiData).length? [img.aiData,text]: [text]
+  );
+  let accumulatedText = ""
+  for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    
+    accumulatedText+= chunkText;
+    setAnswer(accumulatedText)
+  }
+  setImg({
+    isLoading:false,
+    error:"",
+    dbData:{},
+    aiData:{}
+  })
   }
 
   const handleSubmit = async (e) => {
