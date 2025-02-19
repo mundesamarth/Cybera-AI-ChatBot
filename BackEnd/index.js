d71003bde07e8f6086100c76f867715265e249a8
Sam <gentalmen28@gmail.com>
2025-02-19 16:06:53 +0000

    Added a new folder upload.jsx and made changes in newprompt.js this commit is for image upload

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,25 @@
 import express from "express"
+import ImageKit from "imagekit";
+import cors from "cors";
 
 
 const port = process.env.PORT || 3000;  /* Defining the port no port then runs on localhost 3000 */
 const app = express();
 
-app.get("/test",(req,res)=>{    /*Setting up first endpoint */
-    res.send("Its working")
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+     
+}))
+
+const imagekit = new ImageKit({
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
+  });
+
+app.get("/api/upload",(req,res)=>{    /*Setting up first endpoint */
+    const result = imagekit.getAuthenticationParameters();
+    res.send(result);
 })
 
 app.listen(port, ()=> {
