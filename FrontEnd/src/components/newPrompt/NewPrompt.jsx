87aace8784b6c0ce689aff65acd7be582542d431
Sam <gentalmen28@gmail.com>
2025-02-20 02:11:52 +0000

    Getting answer from gemini and storing the data into the database

--- FrontEnd/src/components/newPrompt/NewPrompt.jsx
+++ FrontEnd/src/components/newPrompt/NewPrompt.jsx
@@ -1,23 +1,22 @@
-import { useEffect, useRef, useState } from 'react';
-import './newPrompt.css'
-import Upload from '../upload/Upload';
-import { IKImage } from 'imagekitio-react';
-import model from '../../lib/gemini';
-import Markdown from "react-markdown"
+import { useEffect, useRef, useState } from "react";
+import "./newPrompt.css";
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const NewPrompt = () => {
+const NewPrompt = ({ data }) => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
-  const [question,setQuestion] = useState("")
-  const [answer,setAnswer] = useState("")
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+    aiData: {},
+  });
 
-  const [img,setImg] = useState({
-    isLoading:false,
-    error:"",
-    dbData:{},
-    aiData:{}
-  })
-
-  
   const chat = model.startChat({
     history: [
       {
@@ -29,74 +28,117 @@ const NewPrompt = () => {
         parts: [{ text: "Great to meet you. What would you like to know?" }],
       },
     ],
-    generationConfig:{
-
-    },
+    generationConfig: {},
   });
 
   const endRef = useRef(null);
 
-  useEffect(()=>{
-    endRef.current.scrollIntoView({behavior:"smooth"});
-  },[answer,question,img.dbData])
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [data, answer, question, img.dbData]);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/chats/${data._id}`,
+        {
+          method: "PUT",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            question: question.length ? question : undefined,
+            answer,
+            img: img.dbData?.filePath || undefined,
+          }),
+        }
+      );
+      return await res.json();
+    },
+    onSuccess: (id) => {
+      // Invalidate and refetch
+      queryClient
+        .invalidateQueries({ queryKey: ["chat", data._id] })
+        .then(() => {
+          setQuestion("");
+          setAnswer("");
+          setImg({
+            isLoading: false,
+            error: "",
+            dbData: {},
+            aiData: {},
+          });
+        });
+    },
+    onError: (err) => {
+      console.log(err);
+    },
+  });
 
   const add = async (text) => {
-  setQuestion(text);
-  const result = await chat.sendMessageStream(
-    Object.entries(img.aiData).length? [img.aiData,text]: [text]
-  );
-  let accumulatedText = ""
-  for await (const chunk of result.stream) {
-    const chunkText = chunk.text();
-    
-    accumulatedText+= chunkText;
-    setAnswer(accumulatedText)
-  }
-  setImg({
-    isLoading:false,
-    error:"",
-    dbData:{},
-    aiData:{}
-  })
-  }
+    setQuestion(text);
+
+    try {
+      const result = await chat.sendMessageStream(
+        Object.entries(img.aiData).length ? [img.aiData, text] : [text]
+      );
+      let accumulatedText = "";
+      for await (const chunk of result.stream) {
+        const chunkText = chunk.text();
+
+        accumulatedText += chunkText;
+        setAnswer(accumulatedText);
+      }
+
+      mutation.mutate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const text = e.target.text.value;
-    if(!text) return;
+    if (!text) return;
 
-    add(text)
-  }
+    add(text);
+  };
 
   return (
     <>
-    {/* padding for end chat */}
-    {img.isLoading && <div>Loading.....</div>}
-    {img.dbData?.filePath && (
-      <IKImage 
-        urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
-        path={img.dbData?.filePath}
-        width="380"
-        transformation={[{width:380}]}
-      />
-    )}
+      {/* padding for end chat */}
+      {img.isLoading && <div>Loading.....</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ width: 380 }]}
+        />
+      )}
 
-    {question && <div className='message user'>{question}</div>}
-    {answer && <div className='message'><Markdown>{answer}</Markdown></div>}
+      {question && <div className="message user">{question}</div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
 
-    <div className="endChat" ref={endRef}>
-    </div>
-        <form className='newForm' onSubmit={handleSubmit}>
-          <Upload setImg={setImg}/>
-          <input id='file' type="file" multiple={false} hidden/>
-          <input type="text" name="text" placeholder= "Ask me Anything" />
-          <button>
-            <img src="/arrow.png" alt="" />
-          </button>
-        </form>
+      <div className="endChat" ref={endRef}></div>
+      <form className="newForm" onSubmit={handleSubmit}>
+        <Upload setImg={setImg} />
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" name="text" placeholder="Ask me Anything" />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default NewPrompt
+export default NewPrompt;
